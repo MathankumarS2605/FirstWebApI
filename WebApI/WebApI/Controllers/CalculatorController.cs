23e6f9d37d9cf1ad4662293cb9c974765cd21673
Mathankumar Selvam <mathankumar.s@gavstech.com>
2023-09-21 11:56:50 +0530

    FirstWeb

---
+++ WebApI/WebApI/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // api/Calculator/Add?x=9&y=15
+        [HttpGet("Add")]
+        public int Add(int x,int y)
+        {
+            return x + y+500;
+        }
+
+        //api/Calculator/subtract? x = 100 & y = 15
+        [HttpGet("Sub")]
+        public int Sum(int x, int y)
+        {
+            return x + y + 1000;
+        }
+        // api/Calculator/Multiply?x=100&y=15
+        [HttpGet("Mul")]
+        public int Multiply(int x, int y)
+        {
+            return x * y+899;
+        }
+        // api/Calculator/Division?x=100&y=15
+        [HttpGet("Division")]
+        public int Division(int x, int y)
+        {
+            return x / y+6777;
+        }
+
+    }
+}
